9ffb3af45a85c904017c0f598042654b90377e79
Akira Komamura <akira.komamura@gmail.com>
2022-01-05 16:41:40 +0900

    fix(emacs): Use :global for global bindings

--- emacs/compat.el
+++ emacs/compat.el
@@ -63,9 +63,9 @@
                       (select-window initial-window)))))))
 
 (setup magit
-  (:bind "<f8> <f8>" #'magit-status
-         "<f8> <f9>" #'magit-dispatch
-         "<f8> <f10>" #'magit-file-dispatch))
+  (:global "<f8> <f8>" #'magit-status
+           "<f8> <f9>" #'magit-dispatch
+           "<f8> <f10>" #'magit-file-dispatch))
 
 ;; Local Variables:
 ;; no-byte-compile: t
