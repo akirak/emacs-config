4408ba0f8c5d0d5a8d68cde2bd33d88eb546f964
Akira Komamura <akira.komamura@gmail.com>
2022-01-05 16:42:39 +0900

    feat(emacs): Add magit-stage-file binding

--- emacs/compat.el
+++ emacs/compat.el
@@ -63,7 +63,8 @@
                       (select-window initial-window)))))))
 
 (setup magit
-  (:global "<f8> <f8>" #'magit-status
+  (:global "<f8> <f7>" #'magit-stage-file
+           "<f8> <f8>" #'magit-status
            "<f8> <f9>" #'magit-dispatch
            "<f8> <f10>" #'magit-file-dispatch))
 
