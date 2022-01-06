cf516956fcbd124da1542f9f73ed6b235ad536ed
Akira Komamura <akira.komamura@gmail.com>
2022-01-06 19:00:12 +0900

    feat(emacs): Add tree-sitter
    typescript-mode has been added for testing.

--- emacs/overlay.nix
+++ emacs/overlay.nix
@@ -23,6 +23,9 @@ let
     ];
   };
 
+  elispTreeSitterVersion = "0.16.1";
+  elispTreeSitterLangsVersion = "0.10.13";
+
   makeEmacsConfiguration = initFiles: (emacsTwist {
     inventories = [
       {
@@ -51,10 +54,34 @@ let
           ref = "fix-cl-macs";
         };
       };
+      tsc = _: _: {
+        origin = {
+          type = "github";
+          owner = "emacs-tree-sitter";
+          repo = "elisp-tree-sitter";
+          ref = elispTreeSitterVersion;
+        };
+      };
+      tree-sitter = _: _: {
+        origin = {
+          type = "github";
+          owner = "emacs-tree-sitter";
+          repo = "elisp-tree-sitter";
+          ref = elispTreeSitterVersion;
+        };
+      };
+      tree-sitter-langs = _: _: {
+        origin = {
+          type = "github";
+          owner = "emacs-tree-sitter";
+          repo = "tree-sitter-langs";
+          ref = elispTreeSitterLangsVersion;
+        };
+      };
     };
   }).overrideScope' (self: super: {
-    elispPackages = super.elispPackages // {
-      vterm = super.elispPackages.vterm.overrideAttrs (old: {
+    elispPackages = super.elispPackages.overrideScope' (eself: esuper: {
+      vterm = esuper.vterm.overrideAttrs (old: {
         # Based on the configuration in nixpkgs available at the following URL:
         # https://github.com/NixOS/nixpkgs/blob/af21d41260846fb9c9840a75e310e56dfe97d6a3/pkgs/applications/editors/emacs/elisp-packages/melpa-packages.nix#L483
         nativeBuildInputs = [ final.cmake final.gcc ];
@@ -71,7 +98,58 @@ let
           rm -rf {CMake*,build,*.c,*.h,Makefile,*.cmake}
         '';
       });
-    };
+
+      tsc = esuper.tsc.overrideAttrs (old:
+        let
+          baseUrl = "https://github.com/emacs-tree-sitter/elisp-tree-sitter/releases/download/${elispTreeSitterVersion}";
+          dynName =
+            if prev.system == "x86_64-linux"
+            then "tsc-dyn.so"
+            else throw "Unsupported platform";
+          sha256 = {
+            "tsc-dyn.so" = "0l0gfsvzcra2qwvg241lx7p1m7aiz5sh5kk65prpmqyfg739ascp";
+          };
+        in
+        assert old.version == elispTreeSitterVersion;
+        {
+          tscDyn = builtins.fetchurl {
+            url = "${baseUrl}/${dynName}";
+            sha256 = sha256.${dynName};
+          };
+
+          preBuild = ''
+            cp $tscDyn ${dynName}
+            echo -n "${elispTreeSitterVersion}" > DYN-VERSION
+          '';
+        });
+
+      tree-sitter-langs = esuper.tree-sitter-langs.overrideAttrs (old:
+        let
+          baseUrl =
+            "https://github.com/emacs-tree-sitter/tree-sitter-langs/releases/download/${elispTreeSitterLangsVersion}";
+          os =
+            if prev.system == "x86_64-linux"
+            then "linux"
+            else throw "Unsupported platform";
+          sha256 = {
+            # 0.10.13
+            linux = "1v1db5nlzix4dax769nq5bsrx5fswxafzjqnsci1fxy9v95i53fx";
+            # 0.10.14
+            # linux = "1ngbjnm75wqi8yj0s0jsxxfvypx3c342a9s2b3x10nj659d890wb";
+          };
+        in
+        {
+          bundle = builtins.fetchurl {
+            url = "${baseUrl}/tree-sitter-grammars-${os}-${elispTreeSitterLangsVersion}.tar.gz";
+            sha256 = sha256.${os};
+          };
+
+          preBuild = ''
+            ( mkdir bin && cd bin && tar xzf $bundle )
+          '';
+        }
+      );
+    });
   });
 in
 {
