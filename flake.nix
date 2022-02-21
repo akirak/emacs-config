7bdef29d6e300a15dfbc02950e76886de8e40e56
Akira Komamura <akira.komamura@gmail.com>
2022-02-21 19:42:51 +0900

    chore: Use the latest home-manager from upstream

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     utils.url = "github:numtide/flake-utils";
     flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.1";
 
-    home-manager.url = "github:moinessim/home-manager/nix-2.4-nix-darwin-modules";
+    home-manager.url = "github:nix-community/home-manager";
 
     # Channels
     unstable.url = "nixpkgs/nixos-unstable";
