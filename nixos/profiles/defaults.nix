09cbf61384bdf82954cbdf65973756b2135bdbb4
Akira Komamura <akira.komamura@gmail.com>
2022-04-22 12:50:13 +0900

    fix(nixos): Set nix.linkInputs to true
    This is necessary if you set nix.generateNixPathFromInputs to true.

--- nixos/profiles/defaults.nix
+++ nixos/profiles/defaults.nix
@@ -5,7 +5,7 @@
 }: {
   nix.generateRegistryFromInputs = true;
   nix.generateNixPathFromInputs = true;
-  # nix.linkInputs = true;
+  nix.linkInputs = true;
 
   # Let 'nixos-version --json' know about the Git revision of this
   # flake.
