a00857ebbe79df40b1e8d8cd85e2b81257af7a51
Akira Komamura <akira.komamura@gmail.com>
2022-06-17 01:58:13 +0900

    Use the hash of the unpacked content

--- pkgs/fonts/shippori-mincho.nix
+++ pkgs/fonts/shippori-mincho.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
   name = "shippori-mincho-otf";
   src = fetchzip {
     url = "https://fontdasu.com/download/shippori3.zip";
-    sha256 = "0bi01dxy9s6xl9fizcwixqkv81h9cgcwqsr94vd8n8w21n9app6v";
+    sha256 = "0zh1kj905hw8jmx5kmdsq03y0r4ssad7v36ayh8wmnz45wdfssy0";
     stripRoot = false;
   };
 
