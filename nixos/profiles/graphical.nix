a8ac7d964a22e0e2eeac5f453a160a8fc4f7c2a9
Akira Komamura <akira.komamura@gmail.com>
2022-06-19 18:16:08 +0900

    Run setxkbmap after resuming
    This service should be run every time resuming from suspending.

--- nixos/profiles/graphical.nix
+++ nixos/profiles/graphical.nix
@@ -17,4 +17,15 @@
 
   hardware.pulseaudio.enable = true;
   nixpkgs.config.pulseaudio = true;
+
+  systemd.services.setxkbmap = {
+    enable = true;
+    after = "post-resume.target";
+    description = "Run setxkbmap";
+
+    script = "/run/current-system/sw/bin/setxkbmap -option ctrl:nocaps";
+    environment = {
+      DISPLAY = ":0";
+    };
+  };
 }
