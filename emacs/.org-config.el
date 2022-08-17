5f442e77aeaed5da58b6f811b01858d4ebdc3b50
Akira Komamura <akira.komamura@gmail.com>
2022-08-17 16:23:47 +0900

    Remove all local Org config files and stop using org-starter-mode
    All .org-config.el files, which used to be loaded by my org-starter package,
    have been removed. They are not part of my Emacs init file or ~/org/config.el
    which is tangled from my Org file.
    
    I won't use org-starter package to configure variables, so I don't
    need to turn on org-starter-mode any more.

--- emacs/.org-config.el
+++
@@ -1,7 +0,0 @@
-;; This configuration is written for org-starter:
-;; <https://github.com/akirak/org-starter>
-
-;; <https://github.com/akirak/trivial-elisps>
-
-(org-starter-def "emacs-config.org"
-  :key "e")
--- home/profiles/emacs/extra-init.el
+++ home/profiles/emacs/extra-init.el
@@ -4,22 +4,9 @@
 ;; (require 'poet-theme)
 ;; (load-theme 'poet t)
 
-(eval-after-load 'org
-  (defun akirak/setup-org-starter ()
-    (unless (bound-and-true-p org-starter-path)
-      (setq org-starter-path
-            (thread-last (directory-files-and-attributes "~/" t "^[a-z]")
-                         (cl-remove-if-not
-                          (pcase-lambda (`(,path ,is-dir . ,_))
-                            (and is-dir
-                                 (not (equal (file-name-base path) "result"))
-                                 (file-exists-p (expand-file-name ".org-config.el" path)))))
-                         (mapcar
-                          (lambda (ent) (concat (abbreviate-file-name (car ent)) "/"))))))
-    (when (member "~/config/" org-starter-path)
-      (add-to-list 'org-starter-path "~/config/emacs/"))
-    (require 'org-starter)
-    (org-starter-mode t)))
+(let ((file "~/org/config.el"))
+  (when (file-exists-p file)
+    (load-file file)))
 
 (when (and custom-file (file-exists-p custom-file))
   (load custom-file))
