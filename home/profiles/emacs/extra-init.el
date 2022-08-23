203c1aa772129c0fbc82200045020b6dbb0985b8
Akira Komamura <akira.komamura@gmail.com>
2022-08-24 02:06:02 +0900

    Defer scanning of Org repositories using a timer

--- home/profiles/emacs/extra-init.el
+++ home/profiles/emacs/extra-init.el
@@ -10,3 +10,11 @@
 
 (when (and custom-file (file-exists-p custom-file))
   (load custom-file))
+
+(run-with-timer 2 nil
+                (defun akirak/org-dog-load-on-startup ()
+                  ;; I don't want to hide the init time in the echo area.
+                  (let ((inhibit-message t))
+                    (require 'org-dog nil t)
+                    (when (fboundp 'org-dog-reload-files)
+                      (org-dog-reload-files t)))))
