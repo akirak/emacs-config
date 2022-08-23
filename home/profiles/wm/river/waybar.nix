ad29ff9d40cd5d9828fa79c5f75d65f49d25d2b5
Akira Komamura <akira.komamura@gmail.com>
2022-08-24 01:13:01 +0900

    Add custom waybar configuration

--- home/profiles/wm/river/waybar.nix
+++ home/profiles/wm/river/waybar.nix
@@ -1,5 +1,55 @@
 {
   programs.waybar = {
     enable = true;
+
+    settings = {
+      mainBar = {
+        layer = "top";
+        height = 30;
+        spacing = 4;
+
+        "modules-left" = [
+          # "river/tags"
+        ];
+
+        "modules-center" = [
+          # "river/mode"
+          "river/window"
+        ];
+
+        "modules-right" = [
+          "pulseaudio"
+          "network"
+          "cpu"
+          "memory"
+          "temperature"
+          # "backlight"
+          "keyboard-state"
+          # "sway/language"
+          "battery"
+          "battery#bat2"
+          "clock"
+          "tray"
+        ];
+
+        "keyboard-state" = {
+          numlock = true;
+          capslock = true;
+          format = "{name} {icon}";
+          "format-icons" = {
+            locked = "";
+            unlocked = "";
+          };
+        };
+
+        pulseaudio = {
+          format = "{format_source} {volume}% {icon}";
+        };
+
+        clock = {
+          format = "{:%Y-%m-%d (%a) W%W %H:%M}";
+        };
+      };
+    };
   };
 }
