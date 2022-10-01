a7eb974ab0d3f7bb2bc07d2f9d86e3ad52172d61
Akira Komamura <akira.komamura@gmail.com>
2022-10-01 16:12:49 +0900

    home: Define the git configuration in a separate file

--- home/profiles/development.nix
+++ home/profiles/development.nix
@@ -1,4 +1,8 @@
 {pkgs, ...}: {
+  imports = [
+    ./git
+  ];
+
   home.packages = with pkgs; [
     gh
     pre-commit
@@ -18,70 +22,5 @@
     };
     enableBashIntegration = true;
     enableZshIntegration = true;
-  };
-
-  programs.git = {
-    enable = true;
-
-    # Omit the global user identity
-    # userEmail = "akira.komamura@gmail.com";
-    # userName = "Akira Komamura";
-
-    extraConfig = {
-      github.user = "akirak";
-
-      pull.rebase = false;
-
-      "url \"git@github.com:\"".pushInsteadOf = "https://github.com/";
-
-      core.autocrlf = "input";
-
-      # Only on WSL
-      # core.fileMode = false;
-
-      # Increase the size of post buffers to prevent hung ups of git-push.
-      # https://stackoverflow.com/questions/6842687/the-remote-end-hung-up-unexpectedly-while-git-cloning#6849424
-      http.postBuffer = "524288000";
-    };
-
-    ignores = [
-      ".direnv"
-      "#*"
-    ];
-
-    # Include configuration files to activate contextual identities
-    includes = [
-      {
-        path = "~/.gitconfig";
-      }
-      {
-        path = ./git/default-identity;
-        condition = "hasconfig:remote.*.url:git@git.sr.ht:~akirak/**";
-      }
-      {
-        path = ./git/default-identity;
-        condition = "hasconfig:remote.*.url:https://git.sr.ht/~akirak/**";
-      }
-      {
-        path = ./git/default-identity;
-        condition = "gitdir:~/work2/foss/";
-      }
-      {
-        path = ./git/default-identity;
-        condition = "gitdir:~/work2/personal/";
-      }
-      {
-        path = ./git/default-identity;
-        condition = "gitdir:~/work2/prototypes/";
-      }
-      {
-        path = ./git/default-identity;
-        condition = "gitdir:/assets/";
-      }
-      {
-        path = ./git/default-identity;
-        condition = "gitdir:/git-annex/";
-      }
-    ];
   };
 }
---
+++ home/profiles/git/default.nix
@@ -0,0 +1,66 @@
+{
+  programs.git = {
+    enable = true;
+
+    # Omit the global user identity
+    # userEmail = "akira.komamura@gmail.com";
+    # userName = "Akira Komamura";
+
+    extraConfig = {
+      github.user = "akirak";
+
+      pull.rebase = false;
+
+      "url \"git@github.com:\"".pushInsteadOf = "https://github.com/";
+
+      core.autocrlf = "input";
+
+      # Only on WSL
+      # core.fileMode = false;
+
+      # Increase the size of post buffers to prevent hung ups of git-push.
+      # https://stackoverflow.com/questions/6842687/the-remote-end-hung-up-unexpectedly-while-git-cloning#6849424
+      http.postBuffer = "524288000";
+    };
+
+    ignores = [
+      ".direnv"
+      "#*"
+    ];
+
+    # Include configuration files to activate contextual identities
+    includes = [
+      {
+        path = "~/.gitconfig";
+      }
+      {
+        path = ./default-identity;
+        condition = "hasconfig:remote.*.url:git@git.sr.ht:~akirak/**";
+      }
+      {
+        path = ./default-identity;
+        condition = "hasconfig:remote.*.url:https://git.sr.ht/~akirak/**";
+      }
+      {
+        path = ./default-identity;
+        condition = "gitdir:~/work2/foss/";
+      }
+      {
+        path = ./default-identity;
+        condition = "gitdir:~/work2/personal/";
+      }
+      {
+        path = ./default-identity;
+        condition = "gitdir:~/work2/prototypes/";
+      }
+      {
+        path = ./default-identity;
+        condition = "gitdir:/assets/";
+      }
+      {
+        path = ./default-identity;
+        condition = "gitdir:/git-annex/";
+      }
+    ];
+  };
+}
