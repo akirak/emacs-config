09cbb5bac66fa9aef468a7bb9c78188363db3cd2
Akira Komamura <akira.komamura@gmail.com>
2022-10-14 14:15:32 +0900

    Remove Nix cache settings that are not required in the config
    You can add extra substituters and trusted public keys to individual
    flakes (in nixConfig section), so you don't necessarily have to define
    them in the configuration for NixOS.

--- nixos/profiles/cachix.nix
+++ nixos/profiles/cachix.nix
@@ -16,14 +16,6 @@ in
       trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
     }
     {
-      substituters = ["https://nrdxp.cachix.org"];
-      trusted-public-keys = ["nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4="];
-    }
-    {
-      substituters = ["https://hydra.iohk.io"];
-      trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
-    }
-    {
       substituters = ["https://akirak.cachix.org"];
       trusted-public-keys = ["akirak.cachix.org-1:WJrEMdV1dYyALkOdp/kAECVZ6nAODY5URN05ITFHC+M="];
     }
