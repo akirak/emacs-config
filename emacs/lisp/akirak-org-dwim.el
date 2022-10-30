b0717183d5dd468a078a184cac0093e56e379bca
Akira Komamura <akira.komamura@gmail.com>
2022-10-30 15:59:39 +0900

    lisp/org-dwim: Drop deprecated suffixes

--- emacs/lisp/akirak-org-dwim.el
+++ emacs/lisp/akirak-org-dwim.el
@@ -13,210 +13,6 @@
        (with-current-buffer capture-buffer
          (org-capture-finalize)))))
 
-;;;; Selected files
-
-(defvar akirak-org-dwim-selected-files nil)
-
-(defun akirak-org-dwim--selected-files ()
-  "Return the items of `akirak-org-dwim-selected-files' as a list."
-  (if (stringp akirak-org-dwim-selected-files)
-      (list akirak-org-dwim-selected-files)
-    akirak-org-dwim-selected-files))
-
-;;;; Span
-
-(defcustom akirak-org-dwim-default-span 'day
-  ""
-  :type '(choice (const day)
-                 (const week)))
-
-(defvar akirak-org-dwim-span akirak-org-dwim-default-span)
-
-(defclass akirak-org-dwim-span-class (akirak-transient-variable)
-  ())
-
-(transient-define-infix akirak-org-dwim-switch-span ()
-  :description "Clock sum"
-  :class 'akirak-org-dwim-span-class
-  :variable 'akirak-org-dwim-span)
-
-(cl-defmethod transient-infix-read ((obj akirak-org-dwim-span-class))
-  (cl-case (oref obj value)
-    (day 'week)
-    (week 'day)))
-
-(cl-defmethod transient-format ((obj akirak-org-dwim-span-class))
-  (let* ((value (oref obj value))
-         (range (cl-case value
-                  (day (let ((today (org-clock-special-range 'today)))
-                         (if (time-less-p (current-time) (car today))
-                             (org-clock-special-range 'yesterday)
-                           today)))
-                  (week (let ((thisweek (org-clock-special-range 'thisweek)))
-                          (if (time-less-p (current-time) (car thisweek))
-                              (org-clock-special-range 'lastweek)
-                            thisweek)))))
-         (clocksum (cl-reduce
-                    '+
-                    (mapcar `(lambda (file)
-                               (with-current-buffer (or (find-buffer-visiting file)
-                                                        (find-file-noselect file))
-                                 (org-with-wide-buffer
-                                  (goto-char (point-min))
-                                  (org-clock-sum ',(car range) ',(cadr range)))))
-                            (akirak-org-dwim--selected-files))
-                    :initial-value 0)))
-    (format-spec " %k %d (%v) %c"
-                 `((?k . ,(transient-format-key obj))
-                   (?d . ,(oref obj description))
-                   (?v . ,(cl-case value
-                            (day "daily")
-                            (week "weekly")))
-                   (?c . ,(org-minutes-to-clocksum-string clocksum))))))
-
-;;;; Ql search
-
-(defclass akirak-org-dwim-ql-search-param (akirak-transient-variable)
-  ())
-
-(cl-defmethod transient-infix-read ((obj akirak-org-dwim-ql-search-param))
-  (read--expression (concat (oref obj description) ": ")
-                    (prin1-to-string (oref obj value))))
-
-(cl-defmethod transient-format-value ((obj akirak-org-dwim-ql-search-param))
-  (if-let (value (oref obj value))
-      (concat
-       (propertize "(" 'face 'transient-inactive-value)
-       (propertize (format "%s" value) 'face 'transient-value)
-       (propertize ")" 'face 'transient-inactive-value))
-    ""))
-
-;;;;; Query
-
-(defcustom akirak-org-dwim-ql-query-alist
-  '(("Project backlog"
-     (and (level 2)
-          (parent (heading "Backlog"))
-          (not (tags "ARCHIVE")))))
-  ""
-  :type '(alist :key-type string
-                :value-type (cons (sexp :tag "Query")
-                                  plist
-                                  )))
-
-(defvar akirak-org-dwim-ql-query '(todo))
-
-(defvar akirak-org-dwim-ql-query-name nil)
-
-(defun akirak-org-dwim-read-ql-query-name (&optional prompt initial _history)
-  (completing-read (or prompt "Query: ")
-                   #'akirak-org-dwim-ql-query-completions
-                   nil nil initial))
-
-(defun akirak-org-dwim-ql-query-completions (string pred action)
-  (if (eq action 'metadata)
-      '(metadata . ((annotation-function . akirak-org-dwim-ql-query-annotator)))
-    (complete-with-action action
-                          (mapcar #'car akirak-org-dwim-ql-query-alist)
-                          string
-                          pred)))
-
-(defun akirak-org-dwim-ql-query-annotator (group)
-  (if-let (plist (cdr (assoc group akirak-org-dwim-ql-query-alist)))
-      (concat " " (propertize (prin1-to-string plist)
-                              'face 'font-lock-comment-face))
-    ""))
-
-(defun akirak-org-dwim-ql-new-search ()
-  (interactive)
-  (let ((name (akirak-org-dwim-read-ql-query-name)))
-    (if-let (cell (assoc name akirak-org-dwim-ql-query-alist))
-        (setq akirak-org-dwim-ql-query-name name
-              akirak-org-dwim-ql-query (cadr cell))
-      (setq akirak-org-dwim-ql-query-name nil
-            akirak-org-dwim-ql-query (read name)))
-    (akirak-org-dwim-ql-search)))
-
-(defclass akirak-org-dwim-ql-query-class (transient-suffix)
-  ())
-
-(cl-defmethod transient-format-description ((obj akirak-org-dwim-ql-query-class))
-  (format "%s %s"
-          (oref obj description)
-          (if-let (value akirak-org-dwim-ql-query-name)
-              (concat
-               (propertize "(" 'face 'transient-inactive-value)
-               (propertize (format "%s" value) 'face 'transient-value)
-               (propertize ")" 'face 'transient-inactive-value))
-            "")))
-
-(transient-define-suffix akirak-org-dwim-ql-search ()
-  :class 'akirak-org-dwim-ql-query-class
-  :description "Ql search"
-  (interactive)
-  (if akirak-org-dwim-ql-query
-      (org-ql-search (akirak-org-dwim--selected-files)
-        akirak-org-dwim-ql-query
-        :super-groups (cdr (assoc akirak-org-dwim-super-groups-name
-                                  akirak-org-dwim-super-groups-alist))
-        :sort akirak-org-dwim-ql-sort)
-    (akirak-org-dwim-ql-new-search)))
-
-;;;;; Super groups
-
-(defvar akirak-org-dwim-super-groups-name nil)
-
-(defclass akirak-org-dwim-super-groups-class (akirak-org-dwim-ql-search-param)
-  ())
-
-(defcustom akirak-org-dwim-super-groups-alist
-  (when (require 'org-super-agenda nil t)
-    (mapcar (lambda (kwd)
-              (cons (symbol-name kwd) `((,kwd t))))
-            org-super-agenda-auto-selector-keywords))
-  ""
-  :type '(alist :key-type (string :tag "Description")
-                :value-type (repeat plist)))
-
-(cl-defmethod transient-infix-read ((obj akirak-org-dwim-super-groups-class))
-  (completing-read "Super groups: "
-                   #'akirak-org-dwim-super-groups-completions
-                   nil (oref obj value)))
-
-(defun akirak-org-dwim-super-groups-completions (string pred action)
-  (if (eq action 'metadata)
-      '(metadata . ((annotation-function . akirak-org-dwim-super-group-annotator)))
-    (complete-with-action action
-                          (mapcar #'car akirak-org-dwim-super-groups-alist)
-                          string
-                          pred)))
-
-(defun akirak-org-dwim-super-group-annotator (group)
-  (if-let (plist (cdr (assoc group akirak-org-dwim-super-groups-alist)))
-      (concat " " (propertize (prin1-to-string plist)
-                              'face 'font-lock-comment-face))
-    ""))
-
-(transient-define-infix akirak-org-dwim-super-groups ()
-  :description "Super Groups"
-  :class 'akirak-org-dwim-super-groups-class
-  :variable 'akirak-org-dwim-super-groups-name)
-
-;;;;; Sort
-
-(defvar akirak-org-dwim-ql-sort nil)
-
-(defclass akirak-org-dwim-ql-sort-class (akirak-org-dwim-ql-search-param)
-  ())
-
-(cl-defmethod transient-infix-read ((obj akirak-org-dwim-ql-sort-class))
-  (org-ql-view--complete-sort))
-
-(transient-define-infix akirak-org-dwim-ql-sort ()
-  :description "Ql Sort"
-  :class 'akirak-org-dwim-ql-sort-class
-  :variable 'akirak-org-dwim-ql-sort)
-
 ;;;; Prefix
 
 ;;;###autoload (autoload 'akirak-org-dwim-on-clock "akirak-org-dwim" nil 'interactive)
@@ -231,25 +27,12 @@
    ("o" "Display clocked entry" akirak-org-clock-open)
    ;; Save the current window configuration
    ]
-
-  [:description
-   akirak-org-dwim--files-description
-   :if akirak-org-dwim--files-p
-   [("-s" akirak-org-dwim-switch-span)
-    ("-g" akirak-org-dwim-super-groups)
-    ("-o" akirak-org-dwim-ql-sort)]
-   [("SPC" "Clock in" akirak-org-dwim-clock-in-todo)
-    ("a" "Agenda" akirak-org-dwim-agenda)
-    ("v" akirak-org-dwim-ql-search)
-    ("V" "Ql search with new query" akirak-org-dwim-ql-new-search)]]
-
   [:description
    akirak-org-dwim--memento-block-description
    :if akirak-org-dwim--memento-current-block-p
    :class transient-row
    ("C-o" "Finish" org-memento-finish-block)
    ("C-c" "Stop" org-memento-stop-block)]
-
   [:description
    akirak-org-dwim--memento-description
    :if akirak-org-dwim--memento-p
@@ -262,29 +45,6 @@
    ("C" "Add event" org-memento-add-event)
    ("E" "Check out from the day" org-memento-checkout-from-day
     :if-not akirak-org-dwim--memento-current-block-p)]
-
-  ["Switch focus"
-   :if-not org-clocking-p
-   :class transient-subgroups
-   [:class
-    transient-row
-    ("x" "Add descendants" akirak-org-dwim-expand-selected-files)
-    ("n" "Select a subset" akirak-org-dwim-narrow-selected-files)
-    ("j" "Last clocked file" akirak-org-dwim-select-recent-file)
-    ("/" "Agenda files" akirak-org-dwim-select-agenda-files)
-    ("A" "All agenda files" akirak-org-dwim-select-all-agenda-files)
-    ("\\" "This file" akirak-org-dwim-select-this-file
-     :if (lambda () (derived-mode-p 'org-mode)))]
-   [:class
-    transient-columns
-    :setup-children octopus-setup-context-file-subgroups]]
-
-  ["Clock in"
-   :class transient-row
-   ("I" "Last entry" org-clock-in-last)
-   ("P" "Project" akirak-org-clock-in-to-project)
-   ("J" "History" akirak-consult-org-clock)]
-
   (interactive)
   (transient-setup 'akirak-org-dwim-on-clock))
 
@@ -390,66 +150,7 @@
 (defun akirak-org-dwim--memento-status-description ()
   (format "No current block"))
 
-;;;; Selecting files
-
-(defun akirak-org-dwim-select-agenda-files ()
-  (interactive)
-  (setq akirak-org-dwim-selected-files
-        (completing-read-multiple
-         "Select files: "
-         (org-dog-file-completion :files org-agenda-files)))
-  (transient-setup 'akirak-org-dwim-on-clock))
-
-(defun akirak-org-dwim-select-all-agenda-files ()
-  (interactive)
-  (org-dog-root-add-active-files (cl-case akirak-org-dwim-span
-                                   (week 7)
-                                   (day 1)))
-  (setq akirak-org-dwim-selected-files org-agenda-files)
-  (transient-setup 'akirak-org-dwim-on-clock))
-
-(defun akirak-org-dwim-expand-selected-files ()
-  (interactive)
-  (setq akirak-org-dwim-selected-files
-        (mapcar #'car (org-dog-overview-scan
-                       (akirak-org-dwim--selected-files)
-                       :fast t)))
-  (transient-setup 'akirak-org-dwim-on-clock))
-
-(defun akirak-org-dwim-narrow-selected-files ()
-  (interactive)
-  (setq akirak-org-dwim-selected-files
-        (completing-read-multiple
-         "Select files: "
-         (org-dog-file-completion :files (akirak-org-dwim--selected-files))))
-  (transient-setup 'akirak-org-dwim-on-clock))
-
-(defun akirak-org-dwim-select-recent-file ()
-  (interactive)
-  (when-let (marker (car org-clock-history))
-    (setq akirak-org-dwim-selected-files
-          (abbreviate-file-name (buffer-file-name (marker-buffer marker))))
-    (transient-setup 'akirak-org-dwim-on-clock)))
-
-(defun akirak-org-dwim-select-this-file ()
-  (interactive)
-  (setq akirak-org-dwim-selected-files (abbreviate-file-name (buffer-file-name)))
-  (transient-setup 'akirak-org-dwim-on-clock))
-
 ;;;; Suffix commands
-
-(defun akirak-org-dwim-clock-in-todo ()
-  (interactive)
-  (org-dog-clock-in akirak-org-dwim-selected-files
-                    :query-prefix "todo: "))
-
-(defun akirak-org-dwim-agenda ()
-  (interactive)
-  (let ((org-agenda-files (akirak-org-dwim--selected-files))
-        (org-agenda-span akirak-org-dwim-span)
-        (org-agenda-start-with-clockreport-mode t)
-        (org-agenda-use-time-grid t))
-    (org-agenda nil "a")))
 
 (defun akirak-org-dwim-clock-out ()
   (interactive)
