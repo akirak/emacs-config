654f6dc5c2840b7f40dac8a345853d6302352d7f
Akira Komamura <akira.komamura@gmail.com>
2022-11-21 12:13:22 +0900

    lisp/org-log: Insert a link to the last week entry

--- emacs/lisp/akirak-org-log.el
+++ emacs/lisp/akirak-org-log.el
@@ -8,7 +8,17 @@
   (lambda (start-date end-date)
     (concat (format "[[org-memento:timeline?span=week&date=%s,%s]]\n\n"
                     start-date end-date)
-            "%?"))
+            (format (if-let (marker (akirak-org-log--find-latest-week-entry))
+                        (org-with-point-at marker
+                          (concat (org-link-make-string (concat "id:" (org-id-get-create))
+                                                        "Last week")
+                                  "\n\n"))
+                      ""))
+            "%?\n\n"
+            (format "#+begin_src emacs-lisp
+(org-memento-agenda \"%s\" \"%s\"
+  :super-groups '((:auto-property \"MEMENTO_CATEGORY\")))
+#+end_src" start-date end-date)))
   ""
   :type 'function)
 
@@ -17,23 +27,26 @@
   (interactive "P")
   (if arg
       (call-interactively #'akirak-org-log-insert-new-week-entry)
-    (find-file akirak-org-log-file)
-    (let ((initial-pos (point))
-          ;; To make org-complex-heading-regexp match case-sensitively
-          (case-fold-search nil))
-      (catch 'found-entry
-        (goto-char (point-min))
-        (while (re-search-forward org-complex-heading-regexp nil t)
-          (when (and (string-match-p (regexp-quote "Weekly PPP")
-                                     (match-string 4))
-                     (string-match-p (regexp-quote ":@assessment:")
-                                     (match-string 5)))
-            (org-back-to-heading)
-            (require 'org-ql-find)
-            ;; TODO: Don't depend on org-ql-find
-            (run-hooks 'org-ql-find-goto-hook)
-            (throw 'found-entry t)))
-        (goto-char initial-pos)))))
+    (when-let (marker (akirak-org-log--find-latest-week-entry))
+      (org-goto-marker-or-bmk marker)
+      ;; TODO: Don't depend on org-ql-find
+      (run-hooks 'org-ql-find-goto-hook))))
+
+(defun akirak-org-log--find-latest-week-entry ()
+  (with-current-buffer (or (find-buffer-visiting akirak-org-log-file)
+                           (find-file-noselect akirak-org-log-file))
+    (org-with-wide-buffer
+     ;; To make org-complex-heading-regexp match case-sensitively
+     (let ((case-fold-search nil))
+       (catch 'found-entry
+         (goto-char (point-min))
+         (while (re-search-forward org-complex-heading-regexp nil t)
+           (when (and (string-match-p (regexp-quote "Weekly PPP")
+                                      (match-string 4))
+                      (string-match-p (regexp-quote ":@assessment:")
+                                      (match-string 5)))
+             (org-back-to-heading)
+             (throw 'found-entry (point-marker)))))))))
 
 (defun akirak-org-log-insert-new-week-entry (start-date)
   (interactive (list (org-read-date nil t)))
