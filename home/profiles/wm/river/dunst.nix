e0a7271a751df448845cd20cd2e71e4862a0c824
Akira Komamura <akira.komamura@gmail.com>
2022-12-14 02:02:19 +0900

    home/river: Update dunst settings to conform to the latest spec

--- home/profiles/wm/river/dunst.nix
+++ home/profiles/wm/river/dunst.nix
@@ -8,12 +8,17 @@
     waylandDisplay = "wayland-1";
     settings = {
       global = {
-        monitor = 0;
-        geometry = "600x50-50+65";
-        shrink = "yes";
-        transparency = 10;
+        # monitor = 0;
+        width = 300;
+        height = 300;
+        origin = "top-right";
+        offset = "50x65";
+
         padding = 16;
         horizontal_padding = 16;
+
+        transparency = 10;
+
         font = "JetBrainsMono Nerd Font 10";
         line_height = 4;
         format = ''<b>%s</b>\n%b'';
