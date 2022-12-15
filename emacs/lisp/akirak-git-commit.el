82a0bb0c600d58c2378bbc31008444410d3b5444
Akira Komamura <akira.komamura@gmail.com>
2022-12-16 00:59:44 +0900

    lisp/git-commit: Simplify the implementation

--- emacs/lisp/akirak-git-commit.el
+++ emacs/lisp/akirak-git-commit.el
@@ -26,27 +26,25 @@
                         (plist-get org-store-link-plist :link)
                         (truncate-string-to-width rev 7))))
            (case-fold-search t))
-      (save-current-buffer
-        (org-with-point-at (or (akirak-org-clock--capture-buffer org-clock-hd-marker)
-                               org-clock-hd-marker)
-          (save-excursion
-            (if (re-search-forward akirak-git-commit-log-drawer-start-re
-                                   (org-entry-end-position) t)
-                (progn
-                  (re-search-forward org-drawer-regexp)
-                  (beginning-of-line))
-              ;; It's better for the drawer to not precede a drawer for
-              ;; backlinks (if any), so the commit log drawer should be the last
-              ;; drawer.
-              (org-end-of-meta-data t)
-              (re-search-backward (rx anything "\n") nil t)
-              (goto-char (match-end 0))
-              (insert ":GITCOMMITS:\n:END:\n")
-              (beginning-of-line 0))
-            (insert (akirak-git-commit--build-log-line
-                      :rev-link rev-link
-                      :message message)
-                    "\n")))))))
+      (org-with-point-at org-clock-marker
+        (if (re-search-forward akirak-git-commit-log-drawer-start-re
+                               (org-entry-end-position)
+                               t)
+            (progn
+              (re-search-forward org-drawer-regexp)
+              (beginning-of-line))
+          ;; It's better for the drawer to not precede a drawer for
+          ;; backlinks (if any), so the commit log drawer should be the last
+          ;; drawer.
+          (org-end-of-meta-data t)
+          (re-search-backward (rx anything "\n") nil t)
+          (goto-char (match-end 0))
+          (insert ":GITCOMMITS:\n:END:\n")
+          (beginning-of-line 0))
+        (insert (akirak-git-commit--build-log-line
+                  :rev-link rev-link
+                  :message message)
+                "\n")))))
 
 (cl-defun akirak-git-commit--build-log-line (&key time rev-link message)
   (declare (indent 0))
