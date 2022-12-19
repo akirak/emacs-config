6bbc468a0b2b4493b53050bf2af84019e80baba6
Akira Komamura <akira.komamura@gmail.com>
2022-12-20 04:55:40 +0900

    lisp/org-ql: Add akirak-org-ql-open-link function

--- emacs/lisp/akirak-org-ql.el
+++ emacs/lisp/akirak-org-ql.el
@@ -21,5 +21,35 @@
   :body
   (org-in-archived-heading-p))
 
+(defcustom akirak-org-ql-default-query-prefix "!archived: "
+  ""
+  :type 'string)
+
+;;;###autoload
+(defun akirak-org-ql-find-default (files)
+  (require 'org-ql-find)
+  (org-ql-find files :query-prefix akirak-org-ql-default-query-prefix))
+
+(defvar akirak-org-ql-link-query nil)
+
+;;;###autoload
+(defun akirak-org-ql-open-link (files)
+  "Open a link at a heading from FILES."
+  (require 'org-ql-completing-read)
+  (unless akirak-org-ql-link-query
+    (setq akirak-org-ql-link-query
+          (format "heading-regexp:%s "
+                  ;; org-link-any-re contains a space, which makes it unsuitable
+                  ;; for use in non-sexp org-ql queries.
+                  (rx-to-string `(or (and "http" (?  "s") ":")
+                                     (regexp ,org-link-bracket-re))))))
+  (org-with-point-at (org-ql-completing-read files
+                       :query-prefix (concat akirak-org-ql-default-query-prefix
+                                             akirak-org-ql-link-query))
+    (org-back-to-heading)
+    (org-match-line org-complex-heading-regexp)
+    (goto-char (match-beginning 4))
+    (org-open-at-point)))
+
 (provide 'akirak-org-ql)
 ;;; akirak-org-ql.el ends here
