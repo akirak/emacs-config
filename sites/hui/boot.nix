b66157c03042a5f8b8e32f5c69248d8abfe70f64
Akira Komamura <akira.komamura@gmail.com>
2023-01-22 23:38:29 +0900

    sites/hui: Move the hardware configurations to the module in homelab

--- sites/hui/boot.nix
+++ sites/hui/boot.nix
@@ -6,20 +6,19 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelModules = ["iwlwifi" "acpi_call"];
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    acpi_call
-  ];
-
-  hardware.firmware = [pkgs.wireless-regdb];
-
   environment.systemPackages = [
     pkgs.lshw
     pkgs.git
   ];
-  hardware.enableRedistributableFirmware = true;
 
-  boot.initrd.kernelModules = ["usbcore" "nvme" "sdhci_pci" "mmc_block" "xhci_hcd" "usb-storage"];
+  boot.initrd.kernelModules = [
+    "usbcore"
+    "nvme"
+    "sdhci_pci"
+    "mmc_block"
+    "xhci_hcd"
+    "usb-storage"
+  ];
 
   boot.initrd.luks.devices = {
     cryptroot = {
