ecf8430d4f21e751e367cdbb1156c819ea39477f
Akira Komamura <akira.komamura@gmail.com>
2023-01-23 00:24:52 +0900

    sites/hui: Clean up

--- sites/hui/default.nix
+++ sites/hui/default.nix
@@ -4,18 +4,9 @@ in {
   hostName = "hui";
   inherit username;
 
-  # Turn on wireless card. If it works, move this later to the NixOS module
-  hardware.enableRedistributableFirmware = true;
-
   homeModules = [
     "symlinks"
     "personal"
-  ];
-
-  extraHomeModules = [
-    ({...}: {
-      programs.chromium.enable = false;
-    })
   ];
 
   nixos = {
