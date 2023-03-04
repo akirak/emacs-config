5bafbfe7d2d6e8bd6b078a454f5506358d0a8ddf
Akira Komamura <akira.komamura@gmail.com>
2023-03-04 15:29:44 +0900

    pkgs: Drop packages that have already migrated to my-overlay

--- flake.nix
+++ flake.nix
@@ -227,7 +227,6 @@
           inherit (channels.nixpkgs) readability-cli;
 
           inherit emacs-config;
-          inherit (channels.nixpkgs) emacs-root;
 
           test-emacs-config = channels.nixpkgs.callPackage ./emacs/tests {};
 
