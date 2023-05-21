81338f75747c7d971f24bf0c99304aa1e0e20821
Akira Komamura <akira.komamura@gmail.com>
2023-05-21 11:22:52 +0900

    emacs: Work around lispy to remove swiper and ace-window dependencies
    lispy depends on swiper, which in turn depends on ivy.
    The dependencies are only due to lispy-occur, which I actually don't
    use.

--- emacs/inputs.nix
+++ emacs/inputs.nix
@@ -191,8 +191,20 @@
   };
 
   lispy = _: super: {
-    # le-js depends on indium, which I don't want to install.
-    files = builtins.removeAttrs super.files ["le-js.el"];
+    files = builtins.removeAttrs super.files [
+      # le-js depends on indium, which I don't want to install.
+      "le-js.el"
+      # lispy-occur depends on swiper
+      "lispy-occur.el"
+    ];
+    packageRequires =
+      (builtins.removeAttrs super.packageRequires [
+        "swiper"
+        "ace-window"
+      ])
+      // {
+        avy = "0";
+      };
   };
 
   symbol-overlay = _: _: {
