229698de1af5be89e4889a90a7670e041046e9cc
Akira Komamura <akira.komamura@gmail.com>
2023-05-28 19:23:48 +0900

    lisp/embark: Bind the translation command

--- emacs/lisp/akirak-embark.el
+++ emacs/lisp/akirak-embark.el
@@ -228,6 +228,7 @@
   (define-key embark-file-map "l" #'akirak-embark-load-or-import-file)
   (define-key embark-file-map (kbd "C-c C-T") #'akirak-tailscale-copy-file)
   (define-key embark-region-map (kbd "C-e") #'akirak-embark-goto-region-end)
+  (define-key embark-region-map "V" #'akirak-gpt-translate-vocabulary)
 
   (add-to-list 'embark-target-finders #'akirak-embark-target-org-element)
   (add-to-list 'embark-target-finders #'akirak-embark-target-org-link-at-point)
