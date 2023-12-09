9cdf24d93a9b9ccfbe7f096a95b76be70b321c60
Akira Komamura <akira.komamura@gmail.com>
2023-12-09 23:06:23 +0900

    lisp/git-commit: Perform the check before starting a timer

--- emacs/lisp/akirak-git-commit.el
+++ emacs/lisp/akirak-git-commit.el
@@ -16,7 +16,7 @@
     (remove-hook 'git-commit-post-finish-hook
                  #'akirak-git-commit-log-to-org-clock))))
 
-(cl-defun akirak-git-commit-log-to-org-clock-1 (&key message rev dir)
+(cl-defun akirak-git-commit-log-to-org-clock-1 (&key message rev dir marker)
   (require 'akirak-org-clock)
   (let* ((default-directory dir)
          (rev-link (when (and (require 'orgit nil t)
@@ -24,50 +24,54 @@
                      (org-link-make-string
                       (plist-get org-store-link-plist :link)
                       (truncate-string-to-width rev 7))))
-         (buffer (or (akirak-org-clock--capture-buffer org-clock-marker)
-                     (marker-buffer org-clock-marker)))
          (case-fold-search t))
-    (when (org-dog-buffer-object buffer)
-      (with-current-buffer buffer
-        (when (and (>= (marker-position org-clock-marker) (point-min))
-                   (<= (marker-position org-clock-marker) (point-max)))
-          (save-excursion
-            (goto-char org-clock-marker)
-            (when (equal (org-get-todo-state) "REVIEW")
-              (org-todo "UNDERWAY")
-              (org-set-tags (cl-remove-duplicates
-                             (cons "@failed"
-                                   (org-get-tags nil 'local))
-                             :test #'equal)))
-            (if (re-search-forward akirak-git-commit-log-drawer-start-re
-                                   (org-entry-end-position)
-                                   t)
-                (progn
-                  (re-search-forward org-drawer-regexp)
-                  (beginning-of-line))
-              ;; It's better for the drawer to not precede a drawer for
-              ;; backlinks (if any), so the commit log drawer should be the last
-              ;; drawer.
-              (org-end-of-meta-data t)
-              (when (re-search-backward (rx nonl "\n") nil t)
-                (goto-char (match-end 0)))
-              (insert ":GITCOMMITS:\n:END:\n")
-              (beginning-of-line 0))
-            (insert (akirak-git-commit--build-log-line
-                      :rev-link rev-link
-                      :message message)
-                    "\n")))))))
+    (with-current-buffer (marker-buffer marker)
+      (when (and (>= (marker-position marker) (point-min))
+                 (<= (marker-position marker) (point-max)))
+        (save-excursion
+          (goto-char marker)
+          (when (equal (org-get-todo-state) "REVIEW")
+            (org-todo "UNDERWAY")
+            (org-set-tags (cl-remove-duplicates
+                           (cons "@failed"
+                                 (org-get-tags nil 'local))
+                           :test #'equal)))
+          (if (re-search-forward akirak-git-commit-log-drawer-start-re
+                                 (org-entry-end-position)
+                                 t)
+              (progn
+                (re-search-forward org-drawer-regexp)
+                (beginning-of-line))
+            ;; It's better for the drawer to not precede a drawer for
+            ;; backlinks (if any), so the commit log drawer should be the last
+            ;; drawer.
+            (org-end-of-meta-data t)
+            (when (re-search-backward (rx nonl "\n") nil t)
+              (goto-char (match-end 0)))
+            (insert ":GITCOMMITS:\n:END:\n")
+            (beginning-of-line 0))
+          (insert (akirak-git-commit--build-log-line
+                    :rev-link rev-link
+                    :message message)
+                  "\n"))))))
 
 (defun akirak-git-commit-log-to-org-clock ()
   (when (and (featurep 'org-clock)
              (org-clocking-p))
-    ;; Delay logging so it starts after magit commands finishes
-    (run-with-timer 4 nil
-                    `(lambda ()
-                       (akirak-git-commit-log-to-org-clock-1
-                        :dir ,(magit-toplevel)
-                        :message ,(ring-ref log-edit-comment-ring 0)
-                        :rev ,(magit-rev-parse "HEAD"))))))
+    (let* ((buffer (or (akirak-org-clock--capture-buffer org-clock-marker)
+                       (marker-buffer org-clock-marker)))
+           (obj (org-dog-buffer-object buffer)))
+      (when obj
+        ;; Delay logging so it starts after magit commands finishes
+        (run-with-timer 4 nil
+                        `(lambda ()
+                           (akirak-git-commit-log-to-org-clock-1
+                            :marker ,(set-marker (make-marker)
+                                                 (marker-position org-clock-marker)
+                                                 buffer)
+                            :dir ,(magit-toplevel)
+                            :message ,(ring-ref log-edit-comment-ring 0)
+                            :rev ,(magit-rev-parse "HEAD"))))))))
 
 (cl-defun akirak-git-commit--build-log-line (&key time rev-link message)
   (declare (indent 0))
