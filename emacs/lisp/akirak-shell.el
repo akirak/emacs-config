c7856b043e1d596a4b73a769e797be906b826bbe
Akira Komamura <akira.komamura@gmail.com>
2023-12-23 11:26:49 +0900

    emacs: Transition from vterm to eat

--- emacs/lisp/akirak-shell.el
+++ emacs/lisp/akirak-shell.el
@@ -30,6 +30,18 @@
          (akirak-shell--send-string command)
          (pop-to-buffer (current-buffer)))))))
 
+;;;###autoload
+(defun akirak-shell-run-command-in-some-buffer (command)
+  (if-let (bufs (seq-filter `(lambda (buf)
+                               (eq (buffer-local-value 'major-mode buf)
+                                   'eat-mode))
+                            (buffer-list)))
+      (let ((name (completing-read "Shell: " (mapcar #'buffer-name bufs) nil t)))
+        (with-current-buffer (get-buffer name)
+          (akirak-shell--send-string command)
+          (pop-to-buffer (current-buffer))))
+    (user-error "No shell buffer")))
+
 (defun akirak-shell--send-string (string)
   (pcase (derived-mode-p 'eat-mode)
     (`eat-mode
