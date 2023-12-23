5d1bde5c05e97338d490ef99061114e24101d925
Akira Komamura <akira.komamura@gmail.com>
2023-12-23 11:50:39 +0900

    lisp/shell: Init

---
+++ emacs/lisp/akirak-shell.el
@@ -0,0 +1,42 @@
+;;; akirak-shell.el --- Generic shell wrapper -*- lexical-binding: t -*-
+
+(declare-function eat "ext:eat")
+
+;;;###autoload
+(defalias 'akirak-shell #'eat)
+
+;;;###autoload
+(defalias 'akirak-shell-other-window #'eat-other-window)
+
+;;;###autoload
+(defun akirak-shell-run-command-at-dir (dir command)
+  (pcase (seq-filter `(lambda (buf)
+                        (and (eq (buffer-local-value 'major-mode buf)
+                                 'eat-mode)
+                             (equal (buffer-local-value 'default-directory buf)
+                                    dir)))
+                     (buffer-list))
+    (`nil
+     (let ((default-directory dir))
+       (akirak-shell)
+       (akirak-shell--send-string command)))
+    (`(,buf)
+     (with-current-buffer buf
+       (akirak-shell--send-string command)
+       (pop-to-buffer (current-buffer))))
+    (bufs
+     (let ((name (completing-read "Shell: " (mapcar #'buffer-name bufs) nil t)))
+       (with-current-buffer (get-buffer name)
+         (akirak-shell--send-string command)
+         (pop-to-buffer (current-buffer)))))))
+
+(defun akirak-shell--send-string (string)
+  (pcase (derived-mode-p 'eat-mode)
+    (`eat-mode
+     (eat-term-send-string (buffer-local-value 'eat-terminal (current-buffer))
+                           string))
+    (_
+     (user-error "Not in any of the terminal modes"))))
+
+(provide 'akirak-shell)
+;;; akirak-shell.el ends here
