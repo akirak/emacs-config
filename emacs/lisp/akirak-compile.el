a1598be0cf54a46760eaf45511c6a73f7bd66ebd
Akira Komamura <akira.komamura@gmail.com>
2024-02-11 10:58:33 +0900

    lisp/compile: Add support for Cargo and npm variants

--- emacs/lisp/akirak-compile.el
+++ emacs/lisp/akirak-compile.el
@@ -2,11 +2,43 @@
 
 (defcustom akirak-compile-package-file-alist
   '(("dune-project" . dune)
+    ("Cargo.toml" . cargo)
     ("justfile" . just)
-    ("mix.exs" . mix))
+    ("mix.exs" . mix)
+    ("pnpm-lock.yaml" . pnpm)
+    ("yarn.lock" . yarn)
+    ("package-lock.json" . npm)
+    ("bun.lockb" . bun)
+    ("package.json" . package-json))
   ""
   :type '(alist :key-type (string :tag "File name")
                 :value-type (symbol :tag "Symbol to denote the project type")))
+
+(defcustom akirak-compile-subcommand-alist-alist
+  '((bun
+     ("run" annotation "Run JavaScript with bun, a package.json script, or a bin")
+     ("build" annotation "Build TypeScript and JavaScript into a single file")
+     ("install" annotation "Install dependencies")
+     ("add" annotation "Add a dependency")
+     ("remove" annotation "Remove a dpeendency")
+     ("pm" annotation "More commands for managing packages"))
+    (pnpm
+     ("install" annotation "Install all dependencies for a project")
+     ("add" annotation "Installs a package and any packages that it depends on")
+     ("import" annotation "Generates a pnpm-lock.yaml from an npm package-lock.json")
+     ("remove" annotation "Removes packages from node_modules and from the project's package.json")
+     ("update" annotation "Updates packages to their latest version based on the specified range")
+     ("audit" annotation "Checks for known security issues with the installed packages")
+     ("outdated" annotation "Check for outdated packages")
+     ("exec" annotation "Executes a shell command in scope of a project"))
+    (yarn)
+    (npm
+     ("lock")))
+  ""
+  :type '(alist :key-type (symbol :tag "Backend")
+                :value-type
+                (alist :key-type (string :tag "Subcommand")
+                       :value-type plist)))
 
 (defvar akirak-compile-per-workspace-history
   (make-hash-table :test #'equal :size 10))
@@ -72,7 +104,19 @@
   "Return (command . dir) or command for the next action for PROJECTS."
   (let ((candidates (copy-sequence history)))
     (pcase-dolist (`(,backend . ,dir) projects)
-      (let ((command-alist (akirak-compile--gen-commands-cached backend dir))
+      (let ((command-alist (cond
+                            ((and (eq backend 'package-json)
+                                  (cl-intersection '(pnpm bun yarn npm)
+                                                   (mapcar #'car projects)
+                                                   :test #'eq))
+                             nil)
+                            ((memq backend '(pnpm bun yarn npm))
+                             ;; These backends just read package.json, so the
+                             ;; overhead is small enough to not memoize the
+                             ;; result
+                             (akirak-compile--gen-commands backend dir))
+                            (t
+                             (akirak-compile--gen-commands-cached backend dir))))
             (group (format "%s (%s)" backend (abbreviate-file-name dir))))
         (setq candidates (append candidates (mapcar #'car command-alist)))
         (pcase-dolist (`(,command . ,properties) command-alist)
@@ -106,13 +150,16 @@
           value))))
 
 (defun akirak-compile--gen-commands (backend dir)
-  (pcase backend
-    (`dune
+  (cl-ecase backend
+    (dune
      '(("dune build")
        ("dune build @doc" annotation "Build the documentation ")
        ("dune exec")
        ("opam install ")))
-    (`mix
+    (cargo
+     '(("cargo build")
+       ("cargo run")))
+    (mix
      (let (result)
        (with-temp-buffer
          (akirak-compile--insert-stdout "mix" "help")
@@ -127,7 +174,7 @@
                    result))))
        (cons '("iex -S mix" annotation "Run iex within the context of the application")
              (nreverse result))))
-    (`just
+    (just
      (let ((default-directory dir))
        (with-temp-buffer
          (unless (zerop (call-process "just" nil (list t nil) nil
@@ -140,7 +187,34 @@
            (alist-get 'recipes)
            (mapcar (pcase-lambda (`(,name . ,attrs))
                      `(,(format "just %s" name)
-                       annotation ,(alist-get 'doc attrs))))))))))
+                       annotation ,(alist-get 'doc attrs))))))))
+    ((bun pnpm yarn npm)
+     (let* ((command (symbol-name backend))
+            (script-prefix (concat command
+                                   (when (memq backend '(npm pnpm bun))
+                                     " run")
+                                   " ")))
+       (append (map-apply `(lambda (subcommand body)
+                             (list (concat ,script-prefix subcommand)
+                                   'annotation body))
+                          (with-temp-buffer
+                            (insert-file-contents (expand-file-name "package.json" dir))
+                            (map-elt (json-parse-buffer :array-type 'list) "scripts")))
+               (mapcar `(lambda (ent)
+                          (cons (concat ,command " " (car ent))
+                                (cdr ent)))
+                       (alist-get backend akirak-compile-subcommand-alist-alist)))))
+    (package-json
+     (let ((default-directory dir))
+       (thread-last
+         '(("pnpm" . "pnpm install")
+           ("bun" . "bun install")
+           ("yarn" . "yarn")
+           ("npm" . "npm install"))
+         (seq-filter (lambda (cell)
+                       (executable-find (car cell))))
+         (mapcar (lambda (cell)
+                   (list (cdr cell)))))))))
 
 (defun akirak-compile--insert-stdout (command &rest args)
   "Insert the standard output from a command into the buffer."
