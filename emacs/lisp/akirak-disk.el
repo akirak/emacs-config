cb6201b74131b1fef51cec836b4111f402173bf9
Akira Komamura <akira.komamura@gmail.com>
2024-05-11 19:52:26 +0900

    lisp/disk: Add blkid wrapper

--- emacs/lisp/akirak-disk.el
+++ emacs/lisp/akirak-disk.el
@@ -69,5 +69,131 @@
     (goto-char (point-min))
     (json-parse-buffer :object-type 'alist :array-type 'list)))
 
+;;;###autoload
+(defun akirak-disk-insert-blkid ()
+  (interactive)
+  (let* ((props (akirak-disk--complete-blkid "Select a device: "))
+         (device (cdr (assq 'DEVNAME props)))
+         (candidates (thread-last
+                       props
+                       (mapcar (pcase-lambda (`(,prop . ,value))
+                                 (pcase prop
+                                   (`DEVNAME
+                                    value)
+                                   (`PARTUUID
+                                    (concat "/dev/disk/by-partuuid/" value))
+                                   (`UUID
+                                    (concat "/dev/disk/by-uuid/" value))
+                                   (`LABEL
+                                    (concat "/dev/disk/by-label/" value)))))
+                       (delq nil))))
+    (insert (completing-read (format-prompt "Insert a device path" device)
+                             candidates
+                             nil nil nil nil device))))
+
+(defun akirak-disk--complete-blkid (prompt)
+  (let* ((fdisk (akirak-disk--parse-fdisk-list))
+         (fdisk-disks (cdr (assq 'disks fdisk)))
+         (fdisk-devices (cdr (assq 'devices fdisk)))
+         (alist (mapcar (lambda (props)
+                          (cons (alist-get 'DEVNAME props)
+                                props))
+                        (akirak-disk--parse-blkid))))
+    (cl-labels
+        ((annotator (candidate)
+           (let ((props (cdr (assoc candidate alist))))
+             (concat " "
+                     (or (cdr (assoc candidate fdisk-devices))
+                         (cdr (assoc "Size" (cdr (assoc candidate fdisk-disks)))))
+                     " "
+                     (or (cdr (assq 'TYPE props))
+                         (cdr (assq 'PARTLABEL props)))
+                     (when-let (label (cdr (assq 'LABEL props)))
+                       (format " \"%s\"" label)))))
+         (describe-disk (device)
+           (let* ((info (cdr (assoc device fdisk-disks)))
+                  (size (cdr (assoc "Size" info)))
+                  (model (cdr (assoc "Disk model" info))))
+             (when info
+               (format " (%s%s)"
+                       size
+                       (if model
+                           (concat " " (string-trim-right model))
+                         "")))))
+         (group (candidate transform)
+           (if transform
+               candidate
+             (when-let (group (save-match-data
+                                (pcase candidate
+                                  ((rx bol "/dev/mapper/")
+                                   "dm")
+                                  ((rx bol (group "/dev/nvme" (+ anything)) "p" (+ digit) eol)
+                                   (match-string 1 candidate))
+                                  ((rx bol (group "/dev/" (+ anything)) (+ digit) eol)
+                                   (match-string 1 candidate)))))
+               (concat group (describe-disk group)))))
+         (completions (string pred action)
+           (if (eq action 'metadata)
+               (cons 'metadata
+                     (list (cons 'category 'category)
+                           (cons 'group-function #'group)
+                           (cons 'annotation-function #'annotator)))
+             (complete-with-action action alist string pred))))
+      (cdr (assoc (completing-read prompt #'completions)
+                  alist)))))
+
+(defun akirak-disk--parse-blkid ()
+  (let (group
+        groups)
+    (cl-flet
+        ((add-group ()
+           (push group groups)
+           (setq group nil)))
+      (dolist (s (process-lines "blkid" "-o" "export" "-d"))
+        (if (string-empty-p s)
+            (add-group)
+          (when (string-match (rx bol (group (+ (any "_" alpha)))
+                                  "="
+                                  (group (+ anything)))
+                              s)
+            (push (cons (intern (match-string 1 s))
+                        (match-string 2 s))
+                  group))))
+      (add-group))
+    groups))
+
+(defun akirak-disk--parse-fdisk-list ()
+  (let (disks
+        devices)
+    (with-temp-buffer
+      (call-process "fdisk" nil (list t nil) nil "-l")
+      ;; Collect disk information
+      (goto-char (point-min))
+      (let (device size info)
+        (while (re-search-forward (rx bol "Disk " (group (+? anything)) ": "
+                                      (group (+ digit) "." (+ digit)
+                                             " " (+ alpha) "B"))
+                                  nil t)
+          (setq device (match-string 1)
+                size (match-string 2))
+          (forward-line 1)
+          (while (looking-at (rx bol (group (+? nonl)) ": " (group (+ nonl))))
+            (push (cons (match-string 1)
+                        (match-string 2))
+                  info)
+            (forward-line 1))
+          (push (cons device
+                      (cons (cons "Size" size)
+                            info))
+                disks)))
+      ;; Collect partition information
+      (goto-char (point-min))
+      (while (re-search-forward (rx bol "/dev/" (+ (not (any blank)))) nil t)
+        (push (cons (match-string 0)
+                    (buffer-substring-no-properties (point) (line-end-position)))
+              devices)))
+    `((disks . ,disks)
+      (devices . ,devices))))
+
 (provide 'akirak-disk)
 ;;; akirak-disk.el ends here
