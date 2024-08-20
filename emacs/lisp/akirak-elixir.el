2acce61d87a6853ce829334e72c36adbbd96b1e6
Akira Komamura <akira.komamura@gmail.com>
2024-08-20 12:24:35 +0900

    lisp/elixir: Add a command for closing a tag in heex

--- emacs/lisp/akirak-elixir.el
+++ emacs/lisp/akirak-elixir.el
@@ -43,5 +43,44 @@
             (replace-match new-name t nil nil 1)
             (message "Replaced %s with %s" orig-name new-name)))))))
 
+;;;###autoload
+(defun akirak-elixir-heex-close-tag ()
+  "Close the tag in heex.
+
+This is meant to be a more reliable replacement for `sgml-close-tag'. It
+can also work both in `heex-ts-mode' and inside a sigil template in
+`elixir-ts-mode'."
+  (interactive nil (elixir-ts-mode heex-ts-mode))
+  (let ((start (point)))
+    (unless (looking-at "<")
+      (re-search-backward "<"))
+    (while (not (member (treesit-node-type (treesit-node-at (point)))
+                        '("<" "</")))
+      (re-search-backward "<"))
+    (let ((node (treesit-node-at (point)))
+          name
+          indent-level)
+      (while (not (and (member (treesit-node-type node) '("tag" "component"))
+                       (> (treesit-node-end node) start)))
+        (setq node (treesit-node-parent node)))
+      (catch 'tag-name
+        (dolist (child (treesit-node-children (car (treesit-node-children node))))
+          (when (member (treesit-node-type child) '("tag_name" "component_name"))
+            (setq name (treesit-node-text child))
+            (save-excursion
+              (goto-char (treesit-node-start node))
+              (when (looking-back (rx bol (* blank)) (line-beginning-position))
+                (setq indent-level (- (match-end 0) (match-beginning 0)))))
+            (throw 'tag-name t))))
+      (goto-char start)
+      (when name
+        (when indent-level
+          (if (looking-back (rx bol (* blank)) (line-beginning-position))
+              (beginning-of-line)
+            (newline))
+          (indent-to indent-level))
+        (insert "</" name ">")
+        (delete-trailing-whitespace)))))
+
 (provide 'akirak-elixir)
 ;;; akirak-elixir.el ends here
