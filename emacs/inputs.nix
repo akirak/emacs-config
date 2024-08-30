1ab7a0cfc4570dcb45c949cdb48ec37f9be9191f
Akira Komamura <akira.komamura@gmail.com>
2024-08-30 20:35:10 +0900

    Drop workarounds that I don't need any more

--- emacs/inputs.nix
+++ emacs/inputs.nix
@@ -8,54 +8,24 @@
     };
   };
 
-  graphviz-dot-mode = _: super: {
-    files = builtins.removeAttrs super.files ["company-graphviz-dot.el"];
-  };
-
-  reformatter = _: _: {
-    version = "0.6";
-  };
-
   # Quite a few dired extension packages have missing dependencies.
   dired-collapse = _: super: {
-    packageRequires =
-      {
-        dash = "0";
-        f = "0";
-        dired-hacks-utils = "0";
-      }
-      // super.packageRequires;
+    packageRequires = {
+      dash = "0";
+      f = "0";
+      dired-hacks-utils = "0";
+    } // super.packageRequires;
   };
   dired-filter = _: super: {
-    packageRequires =
-      {
-        dired-hacks-utils = "0";
-        f = "0";
-      }
-      // super.packageRequires;
+    packageRequires = {
+      dired-hacks-utils = "0";
+      f = "0";
+    } // super.packageRequires;
   };
   dired-open = _: super: {
-    packageRequires =
-      {
-        dired-hacks-utils = "0";
-      }
-      // super.packageRequires;
-  };
-  dired-hacks-utils = _: super: {
-    packageRequires =
-      {
-        dash = "0";
-      }
-      // super.packageRequires;
-  };
-
-  lean4-mode = _: _: {
-    origin = {
-      type = "github";
-      owner = "akirak";
-      repo = "lean4-mode";
-      ref = "develop";
-    };
+    packageRequires = {
+      dired-hacks-utils = "0";
+    } // super.packageRequires;
   };
 
   org-bookmark-heading = _: _: {
@@ -116,20 +86,6 @@
     };
   };
 
-  fanyi = _: _: {
-    origin = {
-      type = "github";
-      owner = "akirak";
-      repo = "fanyi.el";
-      ref = "possibly-fix";
-    };
-  };
-  dash-docs = _: super: {
-    files = builtins.removeAttrs super.files [
-      "use-package-dash-docs.el"
-    ];
-  };
-
   lispy = _: super: {
     origin = {
       type = "github";
@@ -153,10 +109,6 @@
       };
   };
 
-  magit-section = _: super: {
-    files = builtins.removeAttrs super.files ["docs/magit-section.texi"];
-  };
-
   org-memento = _: _: {
     origin = {
       type = "github";
@@ -164,14 +116,6 @@
       repo = "org-memento";
       ref = "develop";
     };
-  };
-
-  org = _: super: {
-    origin =
-      super.origin
-      // {
-        ref = "main";
-      };
   };
 
   org-make-toc = _: _: {
@@ -183,56 +127,7 @@
     };
   };
 
-  persist = _: super: {
-    files = builtins.removeAttrs super.files ["persist.texi"];
-  };
-
-  apprentice = _: super: {
-    origin = {
-      type = "github";
-      owner = "akirak";
-      repo = "apprentice";
-      ref = "no-elixir-mode-dep";
-    };
-  };
-
-  lsp-bridge = _: super: {
-    packageRequires =
-      super.packageRequires
-      // {
-        acm = "0";
-        posframe = "0";
-      };
-  };
-
-  acm = _: super: {
-    packageRequires =
-      super.packageRequires
-      // {
-        yasnippet = "0";
-      };
-  };
-
-  org-gcal = _: super: {
-    packageRequires =
-      {
-        dash = "0";
-      }
-      // super.packageRequires;
-  };
-
-  idris-mode = _: super: {
-    files = builtins.removeAttrs super.files ["flycheck-idris.el"];
-  };
-
-  ob-graphql = _: _: {
-    origin = {
-      type = "github";
-      owner = "akirak";
-      repo = "ob-graphql";
-      ref = "graphql-dep";
-    };
-  };
+  persist = _: super: { files = builtins.removeAttrs super.files [ "persist.texi" ]; };
 
   org-autolist = _: _: {
     origin = {
