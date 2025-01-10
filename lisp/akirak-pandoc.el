bdca9c76f211a1b1ec2ca76d24b05ced018380e7
Akira Komamura <akira.komamura@gmail.com>
2025-01-10 12:41:11 +0900

    lisp/pandoc: Add public functions

--- lisp/akirak-pandoc.el
+++ lisp/akirak-pandoc.el
@@ -10,7 +10,7 @@
 (defun akirak-pandoc-save-as-org (begin end)
   "Convert the region to Org and save the result to kill ring."
   (interactive "r")
-  (let ((format (or (akirak-pandoc--buffer-input-format)
+  (let ((format (or (akirak-pandoc-input-format)
                     (completing-read "Input format: "
                                      (akirak-pandoc-input-formats)
                                      nil t)))
@@ -31,13 +31,36 @@
       (delete-file errfile)
       (kill-buffer outbuf))))
 
-(defun akirak-pandoc--buffer-input-format ()
-  "Return a pandoc input format for the buffer."
-  (if (and (eq major-mode 'markdown-mode)
-           (string-match-p "^README\\." (file-name-nondirectory (buffer-file-name))))
-      "gfm"
-    (car (member (string-remove-suffix "-mode" (symbol-name major-mode))
-                 (akirak-pandoc-input-formats)))))
+(cl-defun akirak-pandoc-convert-string (string &key from to)
+  (declare (indent 1))
+  (let ((errfile (make-temp-file "pandoc-errors-")))
+    (unwind-protect
+        (with-temp-buffer
+          (insert string)
+          (unless (zerop (call-process-region
+                          (point-min) (point-max)
+                          akirak-pandoc-executable
+                          'delete (list t errfile)
+                          (concat "--from=" from)
+                          (concat "--to=" to)
+                          "-"))
+            (error "pandoc failed: %s"
+                   (with-temp-buffer
+                     (insert-file-contents errfile)
+                     (buffer-string))))
+          (string-trim (buffer-string)))
+      (delete-file errfile))))
+
+(defun akirak-pandoc-input-format (&optional mode filename)
+  "Return a pandoc input format for the buffer, if any."
+  (let ((mode (or mode major-mode))
+        (filename (file-name-nondirectory
+                   (or filename (buffer-file-name (buffer-base-buffer))))))
+    (if (and (eq mode 'markdown-mode)
+             (string-match-p "^README\\." filename))
+        "gfm"
+      (car (member (string-remove-suffix "-mode" (symbol-name mode))
+                   (akirak-pandoc-input-formats))))))
 
 (defun akirak-pandoc-input-formats ()
   (or akirak-pandoc-input-formats
