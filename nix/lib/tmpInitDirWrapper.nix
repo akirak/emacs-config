c18e10cc762a143e2b7aa0f783c0925a3ba40710
Akira Komamura <akira.komamura@gmail.com>
2025-03-21 18:28:26 +0900

    style: Reformat

--- nix/lib/tmpInitDirWrapper.nix
+++ nix/lib/tmpInitDirWrapper.nix
@@ -4,8 +4,10 @@
 {
   writeShellScriptBin,
   runCommandLocal,
-}: name: emacs-env: let
-  initFile = runCommandLocal "init.el" {} ''
+}:
+name: emacs-env:
+let
+  initFile = runCommandLocal "init.el" { } ''
     mkdir -p $out
     touch $out/init.el
     for file in ${builtins.concatStringsSep " " emacs-env.initFiles}
@@ -15,20 +17,20 @@
     done
   '';
 in
-  writeShellScriptBin name ''
-    set +u
-    set -x
+writeShellScriptBin name ''
+  set +u
+  set -x
 
-    initdir="$(mktemp --tmpdir -d ${name}-XXX)"
+  initdir="$(mktemp --tmpdir -d ${name}-XXX)"
 
-    cleanup() {
-      rm -rf "$initdir"
-    }
+  cleanup() {
+    rm -rf "$initdir"
+  }
 
-    trap cleanup ERR EXIT
+  trap cleanup ERR EXIT
 
-    ln -s ${initFile}/init.el "$initdir/init.el"
-    ln -s ${../../early-init.el} "$initdir/early-init.el"
+  ln -s ${initFile}/init.el "$initdir/init.el"
+  ln -s ${../../early-init.el} "$initdir/early-init.el"
 
-    ${emacs-env}/bin/emacs --init-directory="$initdir" "$@"
-  ''
+  ${emacs-env}/bin/emacs --init-directory="$initdir" "$@"
+''
--- nix/profiles.nix
+++ nix/profiles.nix
@@ -4,35 +4,32 @@
 {
   defaultFeatures,
   lib,
-}: emacs-config: let
-  makeProfile = {
-    presets,
-    personalized,
-    otherSettings,
-  }:
+}:
+emacs-config:
+let
+  makeProfile =
+    {
+      presets,
+      personalized,
+      otherSettings,
+    }:
     lib.nameValuePair
-    "${presets.name}-${otherSettings.name}${
-      if personalized
-      then "-personalized"
-      else ""
-    }"
-    (
-      emacs-config.override (_:
-        {
-          extraFeatures = presets.value;
+      "${presets.name}-${otherSettings.name}${if personalized then "-personalized" else ""}"
+      (
+        emacs-config.override (
+          _:
+          {
+            extraFeatures = presets.value;
 
-          prependToInitFile = ''
-            ;; -*- lexical-binding: t; no-byte-compile: t; -*-
-              (setq custom-file (locate-user-emacs-file "custom.el"))
-              (setq akirak/enabled-status-tags ${
-              if personalized
-              then "t"
-              else "nil"
-            })
-          '';
-        }
-        // otherSettings.value)
-    );
+            prependToInitFile = ''
+              ;; -*- lexical-binding: t; no-byte-compile: t; -*-
+                (setq custom-file (locate-user-emacs-file "custom.el"))
+                (setq akirak/enabled-status-tags ${if personalized then "t" else "nil"})
+            '';
+          }
+          // otherSettings.value
+        )
+      );
 
   matrix = {
     presets = lib.mapAttrsToList lib.nameValuePair {
@@ -54,20 +51,17 @@
     };
   };
 
-  go = acc: {
-    key,
-    values,
-  }:
-    lib.flatten
-    (builtins.map (
-        prev:
-          builtins.map (value: prev // {${key} = value;}) values
-      )
-      acc);
+  go =
+    acc:
+    {
+      key,
+      values,
+    }:
+    lib.flatten (builtins.map (prev: builtins.map (value: prev // { ${key} = value; }) values) acc);
 in
-  lib.pipe matrix [
-    (lib.mapAttrsToList (key: values: {inherit key values;}))
-    (builtins.foldl' go [{}])
-    (builtins.map makeProfile)
-    lib.listToAttrs
-  ]
+lib.pipe matrix [
+  (lib.mapAttrsToList (key: values: { inherit key values; }))
+  (builtins.foldl' go [ { } ])
+  (builtins.map makeProfile)
+  lib.listToAttrs
+]
