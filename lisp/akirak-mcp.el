22d4e323b21b688587b0d6738576db461c7ae41f
Akira Komamura <akira.komamura@gmail.com>
2025-04-24 00:09:11 +0900

    lisp/mcp: Add a Playwright MCP wrapper

---
+++ lisp/akirak-mcp.el
@@ -0,0 +1,33 @@
+;;; akirak-mcp.el ---  -*- lexical-binding: t -*-
+
+(defun akirak-mcp-playwright--response-text (response)
+  (pcase-exhaustive response
+    ((and `(:content ,vec)
+          (let (map :type :text) (elt vec 0))
+          (guard (equal type "text")))
+     text)))
+
+(defun akirak-mcp-playwright--display-text (text)
+  (with-current-buffer (get-buffer-create "*playwright mcp snapshot*")
+    (let ((inhibit-read-only t))
+      (erase-buffer)
+      (insert text))
+    (gfm-view-mode)
+    (goto-char (point-min))
+    (pop-to-buffer (current-buffer))))
+
+;;;###autoload
+(defun akirak-mcp-playwright-snapshot (url)
+  "Display the snapshot of URL."
+  (interactive "sUrl: ")
+  (let ((connection (gethash "playwright-headless" mcp-server-connections)))
+    (unwind-protect
+        (progn
+          (mcp-call-tool connection "browser_navigate" (list :url url))
+          (akirak-mcp-playwright--display-text
+           (akirak-mcp-playwright--response-text
+            (mcp-call-tool connection "browser_snapshot" nil))))
+      (mcp-call-tool connection "browser_close" nil))))
+
+(provide 'akirak-mcp)
+;;; akirak-mcp.el ends here
