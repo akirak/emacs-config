02df5c33b001707b5138a0c1d64e31ceb1377716
Akira Komamura <akira.komamura@gmail.com>
2025-05-05 21:11:53 +0900

    lisp/org-tempo: Support command snippet

--- lisp/akirak-org-tempo.el
+++ lisp/akirak-org-tempo.el
@@ -40,32 +40,38 @@
            (catch 'found-block
              (while (re-search-forward org-babel-src-block-regexp bound t)
                ;; If the source language is tempo, it is a block.
-               (when (equal (match-string 2)
-                            "tempo")
-                 (condition-case-unless-debug nil
-                     (let* ((pos (match-beginning 0))
-                            (element (org-element-at-point pos))
-                            (raw-parameters (org-element-property :parameters element))
-                            (params (org-babel-parse-header-arguments raw-parameters 'no-eval))
-                            (src (org-element-property :value element))
-                            (name (concat prefix tag))
-                            (func (intern (concat "tempo-template-" name))))
-                       (tempo-define-template
-                        name
-                        (read src)
-                        tag
-                        ;; Use the comment at the first line, if any, as the
-                        ;; template description.
-                        (if (string-match (rx bol (* blank) (+ ";")
-                                              (* blank) (group (+ nonl)))
-                                          src)
-                            (match-string 1 src)
-                          (org-entry-get pos "ITEM"))
-                        tag-list)
-                       (akirak-org-tempo--advice-add func params)
-                       (cl-incf count))
-                   (error (message "Error while loading tempo buffer")))
-                 (throw 'found-block t)))
+               (let ((language (match-string 2)))
+                 (when (member language '("tempo" "emacs-lisp" "elisp"))
+                   (condition-case-unless-debug nil
+                       (let* ((pos (match-beginning 0))
+                              (element (org-element-at-point pos))
+                              (raw-parameters (org-element-property :parameters element))
+                              (params (org-babel-parse-header-arguments raw-parameters 'no-eval))
+                              (src (org-element-property :value element))
+                              (name (concat prefix tag))
+                              (func (intern (concat "tempo-template-" name))))
+                         (pcase language
+                           ("tempo"
+                            (tempo-define-template
+                             name
+                             (read src)
+                             tag
+                             ;; Use the comment at the first line, if any, as the
+                             ;; template description.
+                             (if (string-match (rx bol (* blank) (+ ";")
+                                                   (* blank) (group (+ nonl)))
+                                               src)
+                                 (match-string 1 src)
+                               (org-entry-get pos "ITEM"))
+                             tag-list))
+                           ((or "emacs-lisp" "elisp")
+                            (let ((template-name (eval (read src))))
+                              (setq func template-name)
+                              (tempo-add-tag tag func tag-list))))
+                         (akirak-org-tempo--advice-add func params)
+                         (cl-incf count))
+                     (error (message "Error while loading tempo buffer")))
+                   (throw 'found-block t))))
              (message "Not found a tempo block even in the presence of tempo_tag property at %d in %s"
                       (point)
                       (buffer-file-name)))))
