690196508d292a6459f2e416be0ac1d032a0c764
Akira Komamura <akira.komamura@gmail.com>
2025-05-05 15:31:30 +0900

    lisp/effect-ts: Migrate most snippets to Org

--- lisp/akirak-effect-ts.el
+++ lisp/akirak-effect-ts.el
@@ -1,27 +1,22 @@
 ;;; akirak-effect-ts.el --- Minor mode for editing Effect-TS source code -*- lexical-binding: t -*-
 
+(require 'skeleton)
+
 (defvar-keymap akirak-effect-ts-mode-map)
 
 ;;;###autoload (autoload 'akirak-effect-ts-mode "akirak-effect-ts")
 (define-minor-mode akirak-effect-ts-mode
   "A minor mode for editing Effect-TS code efficiently.")
 
-;;;###autoload
+;;;###autoload (autoload 'akirak-effect-ts-insert-yield "akirak-effect-ts")
 (define-skeleton akirak-effect-ts-insert-yield
   "Insert yield*." nil
   "yield* ")
 
-;;;###autoload
+;;;###autoload (autoload 'akirak-effect-ts-insert-gen "akirak-effect-ts")
 (define-skeleton akirak-effect-ts-insert-gen
   "Insert Effect.gen." nil
   "Effect.gen(function* () {" n _ n -2 "})")
-
-;;;###autoload
-(define-skeleton akirak-effect-ts-insert-type-for-schema
-  "Insert a type definition corresponding to a Schema." nil
-  "type " (read-string "Type name: ")
-  " = typeof " (read-string "Schema: ")
-  ".Type")
 
 (provide 'akirak-effect-ts)
 ;;; akirak-effect-ts.el ends here
