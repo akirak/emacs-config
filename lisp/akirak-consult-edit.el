eeee8bac5a37802e1ac0736b382a24ec4944dd8e
Akira Komamura <akira.komamura@gmail.com>
2025-05-06 12:31:10 +0900

    lisp/consult-edit: Add literal candidates

--- lisp/akirak-consult-edit.el
+++ lisp/akirak-consult-edit.el
@@ -7,7 +7,7 @@
   ""
   :type 'function)
 
-(defvar akirak-consult-edit-org-tempo-sources
+(defvar akirak-consult-edit-org-tempo-source
   `(:name "Template functions"
           ;; To suppress the default annotation, choose a different category
           ;; other than @='function.
@@ -23,8 +23,53 @@
                                 (symbol-name (cdr ent)))
                               (tempo-build-collection))))))
 
+(defvar akirak-consult-edit-suggestions nil)
+
+(defvar akirak-consult-edit-suggestions-source
+  `(:name "Suggestions"
+          :category literal
+          :annotate ,(lambda (candidate)
+                       (concat " " (cdr (assoc candidate akirak-consult-edit-suggestions))))
+          :items akirak-consult-generate-suggestions))
+
+(defun akirak-consult-generate-suggestions ()
+  (let ((suggestions
+         (append (when-let* ((filename (if (minibufferp)
+                                           (with-minibuffer-selected-window
+                                             (akirak-consult-edit--filename))
+                                         (akirak-consult-edit--filename))))
+                   `((,filename
+                      . "File name of the buffer.")
+                     (,(file-name-base filename)
+                      . "Base name of the buffer.")
+                     (,(progn
+                         (require 'string-inflection)
+                         (thread-last
+                           (file-name-base filename)
+                           (string-inflection-upper-camelcase-function)))
+                      . "Base name of the buffer, pascal-cased.")
+                     (,(file-relative-name filename (vc-git-root default-directory))
+                      . "Relative path of the file from the root.")))
+                 (remq nil
+                       (list (when-let* ((func (which-function)))
+                               (cons func "Name of the function."))
+                             (cons (format-time-string "%F")
+                                   "Current date.")
+                             (cons user-full-name "Name of the user.")
+                             (when-let* ((pr (project-current)))
+                               (cons (project-name pr)
+                                     "Name of the project.")))))))
+    (setq akirak-consult-edit-suggestions suggestions)
+    (mapcar #'car suggestions)))
+
+(defun akirak-consult-edit--filename ()
+  (if (eq major-mode 'nov-mode)
+      nov-file-name
+    (buffer-file-name (buffer-base-buffer))))
+
 (defvar akirak-consult-edit-sources
-  '(akirak-consult-edit-org-tempo-sources))
+  '(akirak-consult-edit-org-tempo-source
+    akirak-consult-edit-suggestions-source))
 
 ;;;###autoload
 (defun akirak-consult-edit ()
@@ -38,7 +83,9 @@
            (let ((func (intern (car ent))))
              (if (commandp func)
                  (call-interactively func)
-               (funcall func)))))
+               (funcall func))))
+          (literal
+           (insert (car ent))))
       (funcall akirak-consult-edit-fallback-function
                (car ent)))))
 
