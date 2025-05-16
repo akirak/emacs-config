1cd4946d8fb8212d4ac561323776ab2f8150e91a
Akira Komamura <akira.komamura@gmail.com>
2025-05-16 23:27:01 +0900

    lisp/org-hub: Add a command to update the issue/PR subtree

--- lisp/akirak-org-hub.el
+++ lisp/akirak-org-hub.el
@@ -73,5 +73,117 @@
               (alist-get :dir (org-babel-parse-header-arguments value))))
     (seq-uniq)))
 
+;;;###autoload
+(defun akirak-org-hub-update-issue-subtree ()
+  "Update the content of the issue/PR subtree for with latest information."
+  (interactive nil org-mode)
+  (let ((url (or (akirak-org-hub--get-url)
+                 (user-error "Not on a url heading"))))
+    (pcase-exhaustive (or (akirak-org-hub--issue-or-pr-of-url url)
+                          (user-error "Not on an issue/PR url"))
+      ((map :repo :type :number)
+       (let ((data (with-temp-buffer
+                     (unless (call-process akirak-org-hub-gh-program nil (list t nil) nil
+                                           (format "%s" type)
+                                           "view" "--repo" repo
+                                           "--json"
+                                           "author,body,title,closed,closedAt,comments"
+                                           (int-to-string number))
+                       (error "gh command exited with non-zero"))
+                     (goto-char (point-min))
+                     (json-parse-buffer :array-type 'list :object-type 'alist))))
+         (save-excursion
+           (atomic-change-group
+             (org-back-to-heading)
+             (when (org-match-line org-complex-heading-regexp)
+               (replace-match (org-link-make-string url (alist-get 'title data))
+                              nil nil nil 4))
+             (org-end-of-meta-data t)
+             (unless (looking-at org-heading-regexp)
+               (delete-region (point) (org-entry-end-position)))
+             (unless (eolp)
+               (org-open-line 1))
+             (let ((start (point))
+                   (data (alist-get 'body data)))
+               (unless (string-empty-p data)
+                 (insert data)
+                 (akirak-pandoc-replace-with-org start (point))
+                 (goto-char start)))
+             (when (eq t (alist-get 'closed data))
+               (let ((closed-timestamp (thread-last
+                                         (alist-get 'closedAt data)
+                                         (ts-parse)
+                                         (ts-internal)
+                                         (format-time-string (org-time-stamp-format t t)))))
+                 (unless (org-entry-is-done-p)
+                   (org-todo 'done))
+                 (org-back-to-heading)
+                 (if (re-search-forward org-closed-time-regexp (org-entry-end-position) t)
+                     (delete-region (line-beginning-position)
+                                    (line-end-position))
+                   (forward-line 1)
+                   (org-open-line 1))
+                 (insert org-closed-string " " closed-timestamp))))
+           (dolist (comment (alist-get 'comments data))
+             (save-excursion
+               (let ((base-level (org-outline-level))
+                     (bound (save-excursion
+                              (org-end-of-subtree))))
+                 (unless (catch 'found-comment
+                           (while (re-search-forward org-heading-regexp bound t)
+                             (when (string= (akirak-org-hub--get-url)
+                                            (alist-get 'url comment))
+                               (throw 'found-comment t)))
+                           nil)
+                   (goto-char bound)
+                   (atomic-change-group
+                     (newline)
+                     (unless (eolp)
+                       (org-open-line 1))
+                     (let-alist comment
+                       (insert (make-string (org-get-valid-level (1+ base-level))
+                                            ?\*)
+                               " " org-comment-string " "
+                               (org-link-make-string
+                                .url (format "Comment by %s at %s"
+                                             .author.login
+                                             .createdAt))
+                               "\n")
+                       (org-entry-put nil "COMMENTED_AT"
+                                      (thread-last
+                                        (ts-parse .createdAt)
+                                        (ts-internal)
+                                        (format-time-string (org-time-stamp-format t t))))
+                       (org-end-of-meta-data t)
+                       (let ((start (point)))
+                         (insert .body "\n")
+                         (akirak-pandoc-replace-with-org start (point)))))))))))))))
+
+(defun akirak-org-hub--get-url ()
+  (let ((heading (org-entry-get nil "ITEM")))
+    (cond
+     ((string-match org-link-bracket-re heading)
+      (match-string-no-properties 1 heading))
+     ((string-match org-link-plain-re heading)
+      (match-string-no-properties 0 heading))
+     (t
+      (user-error "No matching link")))))
+
+(defun akirak-org-hub--issue-or-pr-of-url (url)
+  (pcase url
+    ((rx bol "https://github.com/" (group (+ (any "-" alnum))
+                                          "/"
+                                          (+ (any "-_." alnum)))
+         "/" (group (or "issues" "pull"))
+         "/" (group (+ (any digit)))
+         eol)
+     (list :repo (match-string 1 url)
+           :type (pcase-exhaustive (match-string 2 url)
+                   ("issues"
+                    'issue)
+                   ("pull"
+                    'pr))
+           :number (string-to-number (match-string 3 url))))))
+
 (provide 'akirak-org-hub)
 ;;; akirak-org-hub.el ends here
