783209b946d2ff7f0a04f4d8f57d62f194c69de6
Akira Komamura <akira.komamura@gmail.com>
2025-08-09 04:23:38 +0900

    lisp/ai-model: Init

---
+++ lisp/akirak-ai-model.el
@@ -0,0 +1,34 @@
+;;; akirak-ai-model.el --- AI models -*- lexical-binding: t -*-
+
+(defcustom akirak-ai-model-list-file
+  (locate-user-emacs-file "ai-model-list.txt")
+  "Path to file that contains a list of models."
+  :type 'file)
+
+;;;###autoload
+(defun akirak-ai-model-list ()
+  (with-temp-buffer
+    (insert-file-contents akirak-ai-model-list-file)
+    (thread-last
+      (string-split (buffer-string) "\n")
+      (cl-remove-if #'string-empty-p)
+      (mapcar #'akirak-ai-model--normalize-name))))
+
+(defun akirak-ai-model--normalize-name (name)
+  "Convert the NAME of a model to the provider/model format."
+  (save-match-data
+    (pcase name
+      ((rx "/")
+       ;; Already normalized
+       name)
+      ((rx bol (or "gpt-"
+                   "codex-"
+                   (and "o" (any digit))))
+       ;; OpenAI (gpt-5, o3, etc.)
+       (concat "openai/" name))
+      (_
+       ;; Fallback
+       name))))
+
+(provide 'akirak-ai-model)
+;;; akirak-ai-model.el ends here
