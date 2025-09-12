4e38223221f0257dce6408cf87f73bda5d2ec1a9
Akira Komamura <akira.komamura@gmail.com>
2025-09-13 05:40:38 +0900

    Configure static commit messages

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((akirak-git-commit-static-message-alist
+          . (((("modified" . "emacs-config.org")) . "emacs: ")
+             ((("modified" . "lisp/akirak-*.el")) . "lisp/\\1: "))))))
