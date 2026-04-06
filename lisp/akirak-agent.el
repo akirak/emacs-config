966766f6226111477c59349c759f5e56068b5eb9
Akira Komamura <akira.komamura@gmail.com>
2026-04-06 18:23:02 +0900

    lisp/agent: Search skills recursively and add the global directory

--- lisp/akirak-agent.el
+++ lisp/akirak-agent.el
@@ -38,6 +38,11 @@
                  (repeat string))
   :group 'akirak)
 
+(defcustom akirak-agent-global-skills-dir "~/.agents/skills/"
+  ""
+  :type 'directory
+  :group 'akirak)
+
 (defun akirak-agent-parse-yaml-header ()
   "Parse YAML front matter in the current buffer into an alist.
 
@@ -66,41 +71,53 @@ alist uses interned symbols as keys."
         (insert-file-contents skill-file)
         (akirak-agent-parse-yaml-header)))))
 
-(cl-defun akirak-agent--skills (root-dir &optional skills-dir)
+(cl-defun akirak-agent--skills (root-dir &optional skills-dir
+                                         &key extra-skills-dir)
   "Return skills in PROJECT-ROOT using CONVENTIONS.
 
 Each element is of the form (NAME . METADATA)."
-  (thread-last
-    (ensure-list (pcase-exhaustive (or skills-dir akirak-agent-skills-dir)
-                   ((pred listp)
-                    (mapcar `(lambda (subdir)
-                               (file-name-concat ,root-dir subdir))
-                            skills-dir))
-                   ((or `nil (pred stringp))
-                    (file-name-concat root-dir (or skills-dir
-                                                   akirak-agent-skills-dir)))))
-    (mapcar (lambda (dir)
-              (directory-files dir 'full "^[A-Za-z0-9]" 'nosort)))
-    (flatten-list)
-    (seq-filter (lambda (dir)
-                  (file-exists-p (file-name-concat dir "SKILL.md"))))
-    (mapcar (lambda (dir)
-              (let* ((metadata (akirak-agent--skill-metadata dir))
+  (cl-assert (string-prefix-p (expand-file-name (file-name-as-directory root-dir))
+                              (expand-file-name default-directory))
+             nil
+             "ROOT-DIR must be an ancestor of default-directory or self")
+  (let ((skills-dirs (ensure-list (or skills-dir akirak-agent-skills-dir)))
+        (cwd default-directory)
+        (extra-skills-dir (or extra-skills-dir
+                              akirak-agent-global-skills-dir))
+        result)
+    (while (not (file-equal-p cwd root-dir))
+      (dolist (absdir (mapcar `(lambda (relative)
+                                 (file-name-concat ,cwd relative))
+                              skills-dirs))
+        (when (file-directory-p absdir)
+          (setq result (append result (akirak-agent--skills-in-dir absdir)))))
+      (setq cwd (file-name-parent-directory cwd)))
+    (when (and extra-skills-dir
+               (file-directory-p extra-skills-dir))
+      (setq result (append result (akirak-agent--skills-in-dir extra-skills-dir))))
+    result))
+
+(defun akirak-agent--skills-in-dir (skills-dir)
+  (let (results)
+    (dolist (skill-dir (directory-files skills-dir 'full "^[A-Za-z0-9]" 'nosort))
+      (when (file-exists-p (file-name-concat skill-dir "SKILL.md"))
+        (push (let* ((metadata (akirak-agent--skill-metadata skill-dir))
                      (name (or (alist-get 'name metadata)
-                               (file-name-base dir))))
-                (cons name metadata))))))
+                               (file-name-base skill-dir))))
+                (cons name metadata))
+              results)))
+    results))
 
 (cl-defun akirak-agent-complete-skill (prompt &optional root-dir &key skills-dir)
   "Complete a skill name in PROJECT-ROOT with PROMPT.
 
 CONVENTIONS controls the relative path to the skills directory.  It may
 be nil, a relative path string, or a list of relative path components."
-  (let* ((root-dir (or root-dir (project-root (project-current))))
+  (let* ((root-dir (or root-dir (vc-git-root default-directory)))
          (skills (akirak-agent--skills root-dir skills-dir))
          (candidates (mapcar #'car skills)))
     (unless candidates
-      (user-error "No skills found in %s"
-                  (akirak-agent--skills-directory root-dir skills-dir)))
+      (user-error "No skills found in %s" root-dir))
     (cl-labels
         ((annotator (candidate)
            (if-let* ((metadata (assoc candidate skills))
