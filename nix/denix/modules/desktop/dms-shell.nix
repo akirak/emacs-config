b7dc6d574ab8f59263293ab8d30e577c7b0fc23f
Akira Komamura <akira.komamura@gmail.com>
2026-06-20 10:21:08 +0900

    denix/modules/niri: Make the config customizable

--- nix/denix/modules/desktop/dms-shell.nix
+++ nix/denix/modules/desktop/dms-shell.nix
@@ -4,6 +4,17 @@ delib.module {
 
   options = with delib; singleEnableOption host.guiFeatured;
 
+  myconfig.ifEnabled = {
+    niri.include = [
+      ''
+        include "dms/alttab.kdl"
+        include "dms/colors.kdl"
+        include "dms/layout.kdl"
+        include "dms/wpblur.kdl"
+      ''
+    ];
+  };
+
   nixos.ifEnabled = {
     # Based on https://danklinux.com/docs/dankmaterialshell/nixos
     programs.dms-shell = {
